e9cc45871236322415c2445c40149dfc618f603b
Saad <saadnanagi@gmail.com>
2018-05-12 22:31:06 +0100

    some repairs

--- starter/src/app/patient/components/uploads/uploads.component.ts
+++ starter/src/app/patient/components/uploads/uploads.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
 import { Upload } from '../../../shared/models/upload';
 
 @Component({
@@ -6,49 +7,14 @@ import { Upload } from '../../../shared/models/upload';
   templateUrl: './uploads.component.html',
   styleUrls: ['./uploads.component.scss']
 })
-export class UploadsComponent implements OnInit, OnChanges {
+export class UploadsComponent {
   @Output() uploadForm = new EventEmitter();
   @Input() uploads: Upload[];
   @Input() index = 0;
-  corporels: Upload[];
-  bacterologie: Upload[];
-  virologie: Upload[];
-  parasitologie: Upload[];
-  uploads$: Upload[];
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
   uploadFile() {
     this.uploadForm.emit();
   }
-
-  ngOnChanges() {
-    this.uploads$ = [];
-    this.bacterologie = [];
-    this.virologie = [];
-    this.corporels = [];
-    this.parasitologie = [];
-    if (this.uploads && this.uploads[0]) {
-      this.uploads.map(image => {
-        if (image.type === 'bacterologie' && this.index === 0) {
-          this.bacterologie.push(image);
-          this.uploads$ = this.bacterologie;
-        } else if (image.type === 'virologie' && this.index === 1) {
-          this.virologie.push(image);
-          console.log(('viiirrr'));
-          this.uploads$ = this.virologie;
-        } else if (image.type === 'fluidCorporels' && this.index === 3) {
-          this.corporels.push(image);
-          this.uploads$ = this.corporels;
-        } else {
-          this.parasitologie.push(image);
-          this.uploads$ = this.parasitologie;
-        }
-      });
-    }
-  }
-
 }
